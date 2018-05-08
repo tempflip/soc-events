daa1a736b112ea0695a684fd62e99cb52606d7c2
Peter Tempfli <tempflip@gmail.com>
2018-05-08 21:03:08 +0200

    w

---
+++ src/aura/Session_Select/Session_SelectHelper.js
@@ -0,0 +1,19 @@
+({
+	getSessions : function(cmp) {
+		var action = cmp.get('c.getSessions');
+
+		action.setCallback(this, function(res) {
+			if (res.getState() != 'SUCCESS') {
+				console.log('### error: ',res.getError());
+				return;
+			}
+			this.setSessions(cmp, res.getReturnValue());
+		});
+		$A.enqueueAction(action);
+	},
+
+	setSessions : function(cmp, sessions) {
+		cmp.set('v.sessions', sessions);
+		console.log(sessions);
+	}
+})
